8f3a75076ee653a1c73665fb814935017165539d
Himanshu-yadav-git <himanshuyadav108108@gmail.com>
2026-09-13 18:27:41 +0530

    calculator code

---
+++ calculator.py
@@ -0,0 +1,55 @@
+def calculator():
+    while True:
+        #Title
+        print("====================================") 
+        print("        PYTHON CALCULATOR")
+        print("====================================")
+
+        #Menu
+
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Division")
+        print("5. Modulus")
+        print("6. Exit")
+        print("====================================")
+
+        #Choice input
+        choice = float(input("Enter your choice :"))
+        if (choice == 6):
+            print("Thanks for using our calculator.")
+            break
+        if choice not in [1,2,3,4,5]:
+            print("❌Invalid choice")
+            continue
+        try:
+            num1 = float(input("Enter your first number :"))
+            num2 = float(input("Enter your second number :"))
+        except ValueError:
+            print("❌ Invalid form")
+            continue
+        if (choice == 1):
+            sum = num1 + num2
+            print("The addition is :",sum)
+        elif (choice == 2):
+            diff = num1 - num2
+            print("The difference is :",diff)
+        elif (choice == 3):
+            multi = num1 * num2
+            print("The multiplication is :",multi)
+        elif (choice == 4):
+            if (num2 == 0):
+                print("❌ Cannot divide by zero")
+                continue
+            else:
+                div = num1 / num2 
+                print("The division is :",div)
+        elif (choice == 5):
+            if (num2 == 0):
+                print("❌ Cannot Divide by zero")
+            else:
+                mod = num1 % num2
+                print("The modulus is :",mod)
+
+calculator()
